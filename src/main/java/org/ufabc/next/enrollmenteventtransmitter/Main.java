b9d06c53b6f7597daaf0820c5c30096a418c3c2d
Thales Paixão <dapaixaothales@gmail.com>
2022-10-18 23:49:51 -0300

    add main class

---
+++ src/main/java/org/ufabc/next/enrollmenteventtransmitter/Main.java
@@ -0,0 +1,12 @@
+package org.ufabc.next.enrollmenteventtransmitter;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+
+    public static void main(String ... args) {
+        Quarkus.run(args);
+    }
+}
