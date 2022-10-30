79b4c8bfef4bafe72f20a50269465bfed120d978
Thales Paixão <dapaixaothales@gmail.com>
2022-10-30 13:41:53 -0300

    add basic controllers
    Co-Authored-By: Guilherme Rocha  <67191432+guiPython@users.noreply.github.com>
    Co-Authored-By: spinajonathan <115479588+spinajonathan@users.noreply.github.com>

--- src/main/java/org/ufabc/next/enrollmenteventtransmitter/infrastructure/rest/StudentController.java
+++ src/main/java/org/ufabc/next/enrollmenteventtransmitter/infrastructure/rest/StudentController.java
@@ -1,11 +1,9 @@
-package org.ufabc.next.enrollmenteventtransmitter.controllers;
+package org.ufabc.next.enrollmenteventtransmitter.infrastructure.rest;
 
 import javax.ws.rs.PathParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
